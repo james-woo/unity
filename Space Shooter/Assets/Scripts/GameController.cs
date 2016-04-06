19697da19471aa93d89bd1f466809831bee3e468
James <jamesjaywoo@gmail.com>
2016-04-06 11:22:52 -0700

    space shooter

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private int score;
+    private bool gameOver;
+    private bool restart;
+
+    void Start()
+    {
+        score = 0;
+        gameOver = false;
+        restart = false;
+        gameOverText.text = "";
+        restartText.text = "";
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+    }
+
+    void Update()
+    {
+        if(restart)
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                //Application.LoadLevel(Application.loadedLevel);
+                SceneManager.LoadScene("Main");
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (!gameOver) {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+            if(gameOver)
+            {
+                restartText.text = "Press 'R' to Restart";
+                restart = true;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
---
+++ Space Shooter/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponController : MonoBehaviour {
+
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public float delay;
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        InvokeRepeating("Fire", delay, fireRate*Random.Range(1.0f, 2.0f));
+	}
+
+    void Fire()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audioSource.Play();
+    }
+	
+}
